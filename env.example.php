da6a6da51da253398e6733077aef7daa3f5cb8a4
Luqman <myarchon02@gmail.com>
2026-05-20 13:40:57 +0700

    perubahan data

---
+++ env.example.php
@@ -0,0 +1,15 @@
+<?php
+
+define('APP_NAME', 'Trafa Coffe');
+define('APP_URL', 'http://localhost/coffee-shop/');
+define('APP_LOCATION', 'Asia/Jakarta');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'coffee');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('PRINTER_BT_MAC', 'Alamat Printer');
+define('PRINTER_BT_CHANNEL', 1);
+define('PRINTER_RFCOMM_DEV', '/dev/rfcomm0');
+define('PRINTER_TIMEOUT', 5);
