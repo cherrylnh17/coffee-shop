f77af9116ae9e65d2a23f183d36c91f4418bc269
Luqman <myarchon02@gmail.com>
2026-05-20 13:42:28 +0700

    perbaikan

--- env.php
+++ env.php
@@ -6,10 +6,10 @@ define('APP_LOCATION', 'Asia/Jakarta');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'coffee');
-define('DB_USER', 'admin');
-define('DB_PASS', 'hakimlnh11');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
-define('PRINTER_BT_MAC', 'DC:0D:51:78:D7:83');
+define('PRINTER_BT_MAC', 'Alamat Printer');
 define('PRINTER_BT_CHANNEL', 1);
 define('PRINTER_RFCOMM_DEV', '/dev/rfcomm0');
 define('PRINTER_TIMEOUT', 5);
